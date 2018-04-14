1da49651ddd86a4c94ff79aa20e6f16e7344d4a5
motestt65677 <pkpk99887766@gmail.com>
2018-04-14 12:43:27 +0800

    remove landing page edit billboards page

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,24 +1,24 @@
-$(document).on('turbolinks:load', function() {
-  var lastFixPos = 0;
-  var threshold = 100;
-  document.onscroll = function() {myScroll()};
+// $(document).on('turbolinks:load', function() {
+//   var lastFixPos = 0;
+//   var threshold = 100;
+//   document.onscroll = function() {myScroll()};
 
-  function myScroll(){
-    var diff = $(window).scrollTop() - lastFixPos;
-    if (diff > threshold){
-      lastFixPos = $(window).scrollTop();
-      document.getElementById('nav-bar').classList.remove('show-nav');
-      document.getElementById('nav-bar').classList.add('hide-nav');
-    }else if (diff < -threshold){
-      lastFixPos = $(window).scrollTop();
-      document.getElementById('nav-bar').classList.remove('hide-nav');
-      document.getElementById('nav-bar').classList.add('show-nav');
+//   function myScroll(){
+//     var diff = $(window).scrollTop() - lastFixPos;
+//     if (diff > threshold){
+//       lastFixPos = $(window).scrollTop();
+//       document.getElementById('nav-bar').classList.remove('show-nav');
+//       document.getElementById('nav-bar').classList.add('hide-nav');
+//     }else if (diff < -threshold){
+//       lastFixPos = $(window).scrollTop();
+//       document.getElementById('nav-bar').classList.remove('hide-nav');
+//       document.getElementById('nav-bar').classList.add('show-nav');
 
-    }
-  }
+//     }
+//   }
 
-  function lightbox(){
-    document.getElementById("lightbox").classList.remove('hidden')
-  }
-});
+//   function lightbox(){
+//     document.getElementById("lightbox").classList.remove('hidden')
+//   }
+// });
 
